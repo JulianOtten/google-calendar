1d3c8f6c6f25d4afa6fc7ef88fa1ccd15f6028bf
Julian Otten <jotten@TWEB.local>
2018-01-12 14:19:30 +0100

    added auto delete old results to index.js

--- index.js
+++ index.js
@@ -198,6 +198,7 @@ function listEvents(auth) {
       });
     } 
   });
+  deleteOld();
 }
  
 /**
@@ -205,9 +206,26 @@ function listEvents(auth) {
  */
 
 function deleteOld() {
+  let date = Date.now();
   connection.query(
-    `SELECT * FROM events WHERE end`,
-    function(err, rows) {
+    `SELECT * FROM events`,
+    function(err, rows) { 
       // code stuff
+      for (let i = 0; i < rows.length; i++) {
+          let r = rows[i];
+          let db_time = r.end_date;
+          let time = new Date(db_time).getTime();
+          let toDelete = r.event_id;
+          if(time < date){
+              console.log(i +": true");
+              connection.query(
+                  `DELETE FROM events WHERE event_id = "${toDelete}"`,
+                  function(err, rows){
+                      if(err) throw err;
+                      console.log("succesfully deleted useless result");
+                  }
+              );
+          }
+      }
     });
 }
